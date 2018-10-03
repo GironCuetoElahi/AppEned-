20bc078078969ae779d758b5edb7cfc55d12cc8c
elahito <elahigironcueto@gmail.com>
2018-10-03 15:07:14 -0500

    version6

--- app/src/main/java/com/example/elahi/aplicacionened/Calendario.java
+++ app/src/main/java/com/example/elahi/aplicacionened/Calendario.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,12 +27,9 @@ public class Calendario extends Fragment {
         view = inflater.inflate(R.layout.fragment_calendario, container, false);
         tabLayout = (TabLayout) view.findViewById(R.id.tablayout);
         appBarLayout = (AppBarLayout) view.findViewById(R.id.appbarid);
-        tabLayout.addTab(tabLayout.newTab().setText("DIA 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("DIA 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("DIA 3"));
-        tabLayout.addTab(tabLayout.newTab().setText("SEMIFINAL"));
-        tabLayout.addTab(tabLayout.newTab().setText("FINAL"));
-
+        viewPager = (ViewPager) view.findViewById(R.id.pager);
+        ViewPagerAdpaterCalendario adapter = new ViewPagerAdpaterCalendario(getFragmentManager());
+        adapter.addFragment(new Fragement_futbol(),"Futbol");
         return view;
     }
 
