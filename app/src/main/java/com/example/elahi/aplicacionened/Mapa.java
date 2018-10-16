2a8fdf7f2af589f1d083fd0e8a81c9aff3c2b9de
JesusAntonioSantaella <geminis_jasc@hotmail.com>
2018-10-16 11:40:49 -0500

    iconos personalizados mapa

--- app/src/main/java/com/example/elahi/aplicacionened/Mapa.java
+++ app/src/main/java/com/example/elahi/aplicacionened/Mapa.java
@@ -13,6 +13,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -44,52 +46,94 @@ public class Mapa extends Fragment implements OnMapReadyCallback {
 
         /*Ubicaciones*/
         // Posicionar el mapa en una localización y con un nivel de zoom
-        LatLng brenamiel = new LatLng(17.09531, -96.74963);
-        LatLng canchamrci = new LatLng(17.07864, -96.71024);
-        LatLng urse = new LatLng(17.0497, -96.6934);
-        LatLng olivos = new LatLng(17.07743, -96.74175);
-        LatLng ito = new LatLng(17.08174, -96.74527);
-        LatLng uabjo = new LatLng(17.04571, -96.71528);
-        LatLng urseBasq = new LatLng(17.052,-96.69259);
-        LatLng harp = new LatLng(16.96318,-96.71181);
-        LatLng estadioMerci = new LatLng(17.01079, -96.57348);
-        LatLng tlacochahuaya = new LatLng(17.00946, -96.59328);
-        LatLng nazareno = new LatLng(17.18064, -96.82133);
-        LatLng salle = new LatLng(17.01961, -96.7203);
-        LatLng vinicio = new LatLng(    17.08915, -96.71062);
+        LatLng brenamiel    = new LatLng(17.09531,-96.74963);
+        LatLng canchamrci   = new LatLng(17.07864,-96.71024);
+        LatLng urse         = new LatLng(17.0497,-96.6934);
+        LatLng olivos       = new LatLng(17.07743,-96.74175);
+        LatLng ito          = new LatLng(17.08174,-96.74527);
+        LatLng uabjo        = new LatLng(17.04571,-96.71528);
+        LatLng urseBasq     = new LatLng(17.052,-96.69259);
+        LatLng harp         = new LatLng(16.96318,-96.71181);
+        LatLng estadioMerci = new LatLng(17.01079,-96.57348);
+        LatLng tlacochahuaya = new LatLng(17.00946,-96.59328);
+        LatLng nazareno     = new LatLng(17.18064,-96.82133);
+        LatLng salle        = new LatLng(17.01961,-96.7203);
+        LatLng vinicio      = new LatLng(17.08915,-96.71062);
         LatLng depBrenamiel = new LatLng(17.10435,-96.75374);
-        // Un zoom mayor que 13 hace que el emulador falle, pero un valor deseado paravcallejero es 17 aprox.
+        // Un zoom mayor que 13 hace que el emulador falle, pero un valor deseado para callejero es 17 aprox.
         float zoom = 13;
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ito, zoom));
         // Colocar un marcador en la misma posición
         googleMap.addMarker(new MarkerOptions().position(brenamiel)
-                .title("Canchas Brenamiel"));
+                .title("Canchas Brenamiel")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Canchas Brenamiel")
+        );
         googleMap.addMarker(new MarkerOptions().position(canchamrci)
-                .title("Canchas MRCI"));
+                .title("Canchas MRCI")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Canchas MRCI")
+        );
         googleMap.addMarker(new MarkerOptions().position(urse)
-                .title("Canchas URSE"));
+                .title("Canchas URSE")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Canchas URSE")
+        );
         googleMap.addMarker(new MarkerOptions().position(olivos)
-                .title("Los Olivos"));
+                .title("Los Olivos")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Los Olivos")
+        );
         googleMap.addMarker(new MarkerOptions().position(ito)
-                .title("Complejo Deportivo Del Instituto Tecnológico De Oaxaca"));
+                .title("Complejo Deportivo Del Instituto Tecnológico De Oaxaca")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Complejo Deportivo Del Instituto Tecnológico De Oaxaca")
+        );
         googleMap.addMarker(new MarkerOptions().position(uabjo)
-                .title("Gimnasio UABJO"));
+                .title("Gimnasio UABJO")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Gimnasio UABJO")
+        );
         googleMap.addMarker(new MarkerOptions().position(urseBasq)
-                .title("Gimnasio URSE"));
+                .title("Gimnasio URSE")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Gimnasio URSE")
+        );
         googleMap.addMarker(new MarkerOptions().position(harp)
-                .title("Academia de Beisbol \"Alfredo Harp Helú\""));
+                .title("Academia de Beisbol \"Alfredo Harp Helú\"")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Academia de Beisbol \"Alfredo Harp Helú\"")
+        );
         googleMap.addMarker(new MarkerOptions().position(estadioMerci)
-                .title("Estadio MRCI"));
+                .title("Estadio MRCI")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Estadio MRCI")
+        );
         googleMap.addMarker(new MarkerOptions().position(tlacochahuaya)
-                .title("Alfombra Azul San Jerónimo Tlacochahuaya"));
+                .title("Alfombra Azul San Jerónimo Tlacochahuaya")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Alfombra Azul San Jerónimo Tlacochahuaya")
+        );
         googleMap.addMarker(new MarkerOptions().position(nazareno)
-                .title("Cancha Nazareno"));
+                .title("Cancha Nazareno")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Cancha Nazareno")
+        );
         googleMap.addMarker(new MarkerOptions().position(salle)
-                .title("La salle"));
+                .title("La salle")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("La salle")
+        );
         googleMap.addMarker(new MarkerOptions().position(vinicio)
-                .title("Parque De Béisbol \"Vinicio Castilla\""));
+                .title("Parque De Béisbol \"Vinicio Castilla\"")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Parque De Béisbol \"Vinicio Castilla\"")
+        );
         googleMap.addMarker(new MarkerOptions().position(depBrenamiel)
-                .title("Club Deportivo Brenamiel A.C."));
+                .title("Club Deportivo Brenamiel A.C.")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.canchas))
+                .snippet("Club Deportivo Brenamiel A.C.")
+        );
 
         /*Hoteles*/
         LatLng misionAngeles    = new LatLng(17.07262,-96.71894);
@@ -109,8 +153,6 @@ public class Mapa extends Fragment implements OnMapReadyCallback {
         LatLng aurora           = new LatLng(17.05838,-96.72543);
         LatLng realhotel        = new LatLng(17.06445,-96.72497);
         LatLng cupulas          = new LatLng(17.10347,-96.71204);
-
-
         LatLng posadacid        = new LatLng(17.07101,-96.71871);
         LatLng crespo           = new LatLng(17.06811,-96.72736);
         LatLng innexpress       = new LatLng(17.07051,-96.71661);
@@ -118,47 +160,110 @@ public class Mapa extends Fragment implements OnMapReadyCallback {
 
 
         googleMap.addMarker(new MarkerOptions().position(misionAngeles)
-                .title("Hotel Misión de los Ángeles"));
+                .title("Hotel Misión de los Ángeles")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Misión de los Ángeles")
+        );
         googleMap.addMarker(new MarkerOptions().position(oaxacaDorado)
-                .title("HOTEL Oaxaca Dorado"));
+                .title("Hotel Oaxaca Dorado")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Oaxaca Dorado")
+        );
         googleMap.addMarker(new MarkerOptions().position(marques)
-                .title("Hotel Marqués Del Valle"));
+                .title("Hotel Marqués Del Valle")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Marqués Del Valle")
+        );
         googleMap.addMarker(new MarkerOptions().position(fortinPlaza)
-                .title("Hotel Fortín Plaza"));
+                .title("Hotel Fortín Plaza")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Fortín Plaza")
+        );
         googleMap.addMarker(new MarkerOptions().position(lagoexpress)
-                .title("Hotel Del Lago Express"));
+                .title("Hotel Del Lago Express")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Del Lago Express")
+        );
         googleMap.addMarker(new MarkerOptions().position(antiguoFor)
-                .title("Hotel Antiguo Fortin"));
+                .title("Hotel Antiguo Fortin")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Antiguo Fortin")
+        );
         googleMap.addMarker(new MarkerOptions().position(oaxacainn)
-                .title("Hotel Oaxaca Inn"));
+                .title("Hotel Oaxaca Inn")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Oaxaca Inn")
+        );
         googleMap.addMarker(new MarkerOptions().position(hotellife)
-                .title("Hotel Life"));
+                .title("Hotel Life")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Life")
+        );
         googleMap.addMarker(new MarkerOptions().position(losolivos)
-                .title("Los Olivos"));
+                .title("Los Olivos")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Los Olivos")
+        );
         googleMap.addMarker(new MarkerOptions().position(virginia)
-                .title("Hotel Virginia"));
+                .title("Hotel Virginia")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Virginia")
+        );
         googleMap.addMarker(new MarkerOptions().position(riveraA)
-                .title("Hotel Rivera del Ángel"));
+                .title("Hotel Rivera del Ángel")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Rivera del Ángel")
+        );
         googleMap.addMarker(new MarkerOptions().position(marquesado)
-                .title("Hotel del Marquesado"));
+                .title("Hotel del Marquesado")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel del Marquesado")
+        );
         googleMap.addMarker(new MarkerOptions().position(xadami)
-                .title("Suites Xadani"));
+                .title("Suites Xadani")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Suites Xadani")
+        );
         googleMap.addMarker(new MarkerOptions().position(ciruelos)
-                .title("Hotel Los Ciruelos"));
+                .title("Hotel Los Ciruelos")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Los Ciruelos")
+        );
         googleMap.addMarker(new MarkerOptions().position(aurora)
-                .title("Hotel Aurora"));
+                .title("Hotel Aurora")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Aurora")
+        );
         googleMap.addMarker(new MarkerOptions().position(realhotel)
-                .title("Oaxaca Real Hotel"));
+                .title("Oaxaca Real Hotel")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Oaxaca Real Hotel")
+        );
         googleMap.addMarker(new MarkerOptions().position(cupulas)
-                .title("Hotel Las Cúpulas"));
+                .title("Hotel Las Cúpulas")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Las Cúpulas")
+        );
         googleMap.addMarker(new MarkerOptions().position(posadacid)
-                .title("Hotel Posada el Cid"));
+                .title("Hotel Posada el Cid")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Posada el Cid")
+        );
         googleMap.addMarker(new MarkerOptions().position(crespo)
-                .title("Hotel Parador Crespo"));
+                .title("Hotel Parador Crespo")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Parador Crespo")
+        );
         googleMap.addMarker(new MarkerOptions().position(innexpress)
-                .title("Hotel Oaxaca Inn Express"));
+                .title("Hotel Oaxaca Inn Express")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Oaxaca Inn Express")
+        );
         googleMap.addMarker(new MarkerOptions().position(huautla)
-                .title("Hotel Huautla"));
+                .title("Hotel Huautla")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hotel))
+                .snippet("Hotel Huautla")
+        );
 
         // Más opciones para el marcador en:
         // https://developers.google.com/maps/documentation/android/marker
